1bd8601209fdd2531ce8231c470f67c89d352601
gianni <g.defabritiis@gmail.com>
2019-09-09 02:01:24 +0200

    added monitor

--- animal/monitor_exp.py
+++ animal/monitor_exp.py
@@ -12,7 +12,8 @@ from baselines.bench import load_results
 from matplotlib import pylab as plt
 import numpy as np
 
-exps = glob('exp_*')
+exps = glob('RUNS/exp_2*')
+print(exps)
 
 while True:
     for i,d in enumerate(exps):
@@ -20,51 +21,35 @@ while True:
         try:
             df = load_results(d)
             df['f']= df['l'].cumsum()/1000000
+            df['perf']= df['r']/(2+df['max_reward'])#correct for reshaping, but I should really have the real ones in info
+            df['perf'].where(df['perf']>0,0,inplace=True)
+            df['goal'] = df['perf']>0.9  #guess a threadshold
+            print(df['goal'].iloc[0:100])
+
             roll = 5 
-            rdf = df.rolling(roll)
             total_time = df['t'].iloc[-1]
             total_steps = df['l'].sum()
             total_episodes = df['r'].size
             
             ax = plt.subplot(2, 2, 1)
             ax.set_title(' {} total time: {:.1f} h FPS {:.1f}'.format(d.upper(),total_time/3600, total_steps/total_time))
-
-            rdf.max().iloc[0:-1:40].plot('f','floor', style='-',ax=ax,legend=False)
-            rdf.min().iloc[0:-1:40].plot('f','floor', style='-',ax=ax,legend=False)
-            df.rolling(50*roll).mean().iloc[0:-1:40].plot('f','floor', style='-', ax=ax,legend=False)
-            ax.yaxis.set_major_locator(plt.MultipleLocator(1))
-            ax.xaxis.set_minor_locator(plt.MultipleLocator(2))
-            ax.set_xlabel('N. steps (M)')
-            ax.set_ylabel('Floor')
-            ax.grid(True)
-
-            ax = plt.subplot(2, 2, 2)
-            rdf.max().iloc[0:-1:40].plot('f','r', ax=ax,legend=False)
-            rdf.min().iloc[0:-1:40].plot('f','r', ax=ax,legend=False)
-            df.rolling(50*roll).mean().iloc[0:-1:40].plot('f','r', ax=ax,legend=False)
+            df[['f','r']].rolling(50*roll).mean().iloc[0:-1:40].plot('f','r',  ax=ax,legend=False)
             ax.set_xlabel('N. steps (M)')
             ax.set_ylabel('Reward')
             ax.grid(True)
-            
-            ax = plt.subplot(2, 2, 3)
-            ax.set_title('Goal mean: {:.5f}'.format(df['goal'].iloc[-5000:].mean()))
-            rdf.max().iloc[0:-1:40].plot('f','goal', style='-',ax=ax,legend=False)
-            rdf.min().iloc[0:-1:40].plot('f','goal', style='-',ax=ax,legend=False)
-            df.rolling(50*roll).mean().iloc[0:-1:40].plot('f','goal', style='-', ax=ax,legend=False)
-            ax.yaxis.set_major_locator(plt.MultipleLocator(0.1))
-            ax.xaxis.set_minor_locator(plt.MultipleLocator(2))
+    
+            ax = plt.subplot(2, 2, 2)
+            df[['f','perf']].rolling(50*roll).mean().iloc[0:-1:40].plot('f','perf', ax=ax,legend=False)
             ax.set_xlabel('N. steps (M)')
-            ax.set_ylabel('Goal')
+            ax.set_ylabel('Performance')
             ax.grid(True)
 
-            ax = plt.subplot(2, 2, 4)
-            rdf.max().iloc[0:-1:40].plot(y='l', ax = ax,legend=False)
-            rdf.min().iloc[0:-1:40].plot(y='l', ax = ax,legend=False)
-            df.rolling(50*roll).mean().iloc[0:-1:40].plot(y='l', ax = ax,legend=False)
-            ax.set_xlabel('N. episodes')
-            ax.set_ylabel('N. steps')
+            ax = plt.subplot(2, 2, 3)
+            df[['f','goal']].rolling(50*roll).mean().iloc[0:-1:40].plot('f','goal', ax=ax,legend=False)
+            ax.set_xlabel('N. steps (M)')
+            ax.set_ylabel('Performance')
             ax.grid(True)
-
+             
             fig.tight_layout() 
             ax.get_figure().savefig('/webdata/'+d+'.jpg')
         except:
