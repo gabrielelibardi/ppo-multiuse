96b58193121f26499c1dbf15a9b4f819be643a73
albertbou92 <albertbo@kth.se>
2019-10-17 12:04:59 +0200

    fix bug

--- animal/vision_module/vision_visualize_data.py
+++ animal/vision_module/vision_visualize_data.py
@@ -37,8 +37,8 @@ def log_plots(log_dir, train_data, test_data, samples=100):
             obs, pos, rot = data
 
             obs = obs[0, :, :, :].permute(1, 2, 0).numpy()
-            pos = pos.numpy()
-            rot = rot.numpy()
+            pos = pos.numpy().squeeze()
+            rot = rot.numpy().squeeze()
 
             fig = plot_sample(obs, pos, rot)
 
