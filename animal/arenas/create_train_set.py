27d754da8895c36922ddba9da90d6d8482eb0576
albertbou92 <albertbo@kth.se>
2019-10-18 15:07:17 +0200

    fix bug

--- animal/arenas/create_train_set.py
+++ animal/arenas/create_train_set.py
@@ -59,7 +59,7 @@ if __name__ == '__main__':
     # c1
     for i in range(1, 201):
             create_c1_arena(arguments.target_dir if arguments.unify
-                            else "{}/preferences/".format(arguments.target_dir),
+                            else "{}preferences/".format(arguments.target_dir),
                             'c1_{}'.format(str(i).zfill(4)),
                             max_reward=float(np.random.randint(5, 10)),
                             time=random.choice([250, 500]), is_train=True)
@@ -67,7 +67,7 @@ if __name__ == '__main__':
     # c2
     for i in range(1, 201):
             create_c2_arena(arguments.target_dir if arguments.unify
-                            else "{}/preferences/".format(arguments.target_dir),
+                            else "{}preferences/".format(arguments.target_dir),
                             'c2_{}'.format(str(i).zfill(4)),
                             max_reward=float(np.random.randint(5, 10)),
                             time=random.choice([250, 500]), is_train=True,
@@ -76,13 +76,13 @@ if __name__ == '__main__':
     # c3
     for i in range(1, 201):
         create_c3_arena(arguments.target_dir if arguments.unify
-                        else "{}/navigate/", 'c3_{}'.format(
+                        else "{}navigate/", 'c3_{}'.format(
             str(i).zfill(4)), time=random.choice([500, 1000]), is_train=True)
 
     # c3
     for i in range(501, 701):
         create_c3_arena_basic(arguments.target_dir if arguments.unify
-                              else "{}/navigate/".format(arguments.target_dir),
+                              else "{}navigate/".format(arguments.target_dir),
                               'c3_{}'.format(str(i).zfill(4)),
                               time=random.choice([500, 1000]),
                               num_walls=np.random.randint(5, 15),
@@ -91,7 +91,7 @@ if __name__ == '__main__':
     # c4
     for i in range(1, 501):
         create_c4_arena(arguments.target_dir if arguments.unify
-                        else "{}/zones/".format(arguments.target_dir),
+                        else "{}zones/".format(arguments.target_dir),
                         'c4_{}'.format(str(i).zfill(4)),
                         time=random.choice([500, 1000]),
                         num_red_zones=8, max_orange_zones=3, is_train=True)
@@ -99,21 +99,21 @@ if __name__ == '__main__':
     # c5
     for i in range(1, 500):
         create_c5_arena(arguments.target_dir if arguments.unify
-                        else "{}/ramps/".format(arguments.target_dir),
+                        else "{}ramps/".format(arguments.target_dir),
                         'c5_{}'.format(str(i).zfill(4)),
                         time=random.choice([500, 1000]), is_train=True)
 
     # c6
     for i in range(1, 201):
         create_c6_arena(arguments.target_dir if arguments.unify
-                        else "{}/generalize/".format(arguments.target_dir),
+                        else "{}generalize/".format(arguments.target_dir),
                         'c6_{}'.format(str(i).zfill(4)),
                         time=random.choice([500, 1000]), is_train=True)
 
     # c6
     for i in range(501, 701):
         create_c6_arena_basic(arguments.target_dir if arguments.unify
-                              else "{}/generalize/".format(arguments.target_dir),
+                              else "{}generalize/".format(arguments.target_dir),
                               'c6_{}'.format(str(i).zfill(4)),
                               time=random.choice([500, 1000]), is_train=True,
                               num_walls=np.random.randint(5, 15))
@@ -121,14 +121,14 @@ if __name__ == '__main__':
     # c7
     for i in range(1, 301):
         create_c7_arena(arguments.target_dir if arguments.unify
-                        else "{}/internal_model/".format(arguments.target_dir),
+                        else "{}internal_model/".format(arguments.target_dir),
                         'c7_{}'.format(str(i).zfill(4)),
                         time=random.choice([500, 1000]), is_train=True)
 
     # mazes
     for i in range(1, 1001):
         create_maze(arguments.target_dir if arguments.unify
-                    else "{}/mazes/".format(arguments.target_dir),
+                    else "{}mazes/".format(arguments.target_dir),
                     'c8_{}'.format(str(i).zfill(4)),
                     time=random.choice([500, 1000]), num_cells=2,
                     obj=random.choice(['CylinderTunnel', 'door', 'Cardbox1']),
@@ -137,21 +137,21 @@ if __name__ == '__main__':
     # choice
     for i in range(1, 400):
         create_arena_choice(arguments.target_dir if arguments.unify
-                            else "{}/choices/".format(arguments.target_dir),
+                            else "{}choices/".format(arguments.target_dir),
                             'c9_{}'.format(str(i).zfill(4)),
                             time=random.choice([500, 1000]), is_train=True)
 
     # cross
     for i in range(1, 501):
         create_arena_cross(arguments.target_dir if arguments.unify
-                           else "{}/navigate/".format(arguments.target_dir),
+                           else "{}navigate/".format(arguments.target_dir),
                            'c10_{}'.format(str(i).zfill(4)),
                            time=random.choice([500, 1000]), is_train=True)
 
     # push1
     for i in range(501, 1001):
         create_arena_push1(arguments.target_dir if arguments.unify
-                           else "{}/push/".format(arguments.target_dir),
+                           else "{}push/".format(arguments.target_dir),
                            'c10_{}'.format(str(i).zfill(4)),
                            time=random.choice([500, 1000]),
                            is_train=True)
@@ -159,7 +159,7 @@ if __name__ == '__main__':
     # push2
     for i in range(1001, 1501):
         create_arena_push2(arguments.target_dir if arguments.unify
-                           else "{}/push/".format(arguments.target_dir),
+                           else "{}push/".format(arguments.target_dir),
                            'c10_{}'.format(str(i).zfill(4)),
                            time=random.choice([500, 1000]),
                            is_train=True)
@@ -167,14 +167,14 @@ if __name__ == '__main__':
     # tunnel1
     for i in range(1501, 2001):
         create_arena_tunnel1(arguments.target_dir if arguments.unify
-                             else "{}/tunnels/".format(arguments.target_dir),
+                             else "{}tunnels/".format(arguments.target_dir),
                              'c10_{}'.format(str(i).zfill(4)),
                              time=random.choice([500, 1000]),
                              is_train=True)
     # tunnel2
     for i in range(2001, 2501):
         create_arena_tunnel2(arguments.target_dir if arguments.unify
-                             else "{}/tunnels/".format(arguments.target_dir),
+                             else "{}tunnels/".format(arguments.target_dir),
                              'c10_{}'.format(str(i).zfill(4)),
                              time=random.choice([500, 1000]),
                              is_train=True)
@@ -182,21 +182,21 @@ if __name__ == '__main__':
     # ramp1
     for i in range(2501, 3001):
         create_arena_ramp1(arguments.target_dir if arguments.unify
-                           else "{}/ramps/".format(arguments.target_dir),
+                           else "{}ramps/".format(arguments.target_dir),
                            'c10_{}'.format(str(i).zfill(4)),
                            time=random.choice([500, 1000]),
                            is_train=True)
     # ramp2
     for i in range(3001, 3501):
         create_arena_ramp2(arguments.target_dir if arguments.unify
-                           else "{}/ramps/".format(arguments.target_dir),
+                           else "{}ramps/".format(arguments.target_dir),
                            'c10_{}'.format(str(i).zfill(4)),
                            time=random.choice([500, 1000]),
                            is_train=True)
     # ramp3
     for i in range(3501, 4001):
         create_arena_ramp3(arguments.target_dir if arguments.unify
-                           else "{}/ramps/".format(arguments.target_dir),
+                           else "{}ramps/".format(arguments.target_dir),
                            'c10_{}'.format(str(i).zfill(4)),
                            time=random.choice([500, 1000]),
                            is_train=True)
@@ -204,7 +204,7 @@ if __name__ == '__main__':
     # narrow1
     for i in range(4001, 4501):
         create_arena_narrow_spaces_1(arguments.target_dir if arguments.unify
-                                     else "{}/narrow/".format(arguments.target_dir),
+                                     else "{}narrow/".format(arguments.target_dir),
                                      'c10_{}'.format(str(i).zfill(4)),
                                      time=random.choice([500, 1000]),
                                      is_train=True)
@@ -212,7 +212,7 @@ if __name__ == '__main__':
     # narrow1
     for i in range(4501, 5001):
         create_arena_narrow_spaces_2(arguments.target_dir if arguments.unify
-                                     else "{}/narrow/".format(arguments.target_dir),
+                                     else "{}narrow/".format(arguments.target_dir),
                                      'c10_{}'.format(str(i).zfill(4)),
                                      time=random.choice([500, 1000]),
                                      is_train=True)
