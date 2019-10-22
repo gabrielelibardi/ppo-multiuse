fd65806d690e2da630b439c698f9687411905e0f
albertbou92 <albertbo@kth.se>
2019-10-22 21:27:16 +0200

    fix bug

--- animal/object_detection_module/object_arenas.py
+++ animal/object_detection_module/object_arenas.py
@@ -1,43 +1,6 @@
 import random
+from .settings import objects, labels
 from animal.arenas.utils.edit_arenas import add_object, write_arena
-
-objects = [
-    'GoodGoal',
-    'BadGoal',
-    'GoodGoalMulti',
-    'Wall',
-    'Ramp',
-    'CylinderTunnel',
-    'WallTransparent',
-    'CylinderTunnelTransparent',
-    'Cardbox1',
-    'Cardbox2',
-    'UObject',
-    'LObject',
-    'LObject2',
-    'DeathZone',
-    'HotZone'
-]
-
-labels = {
-    'GoodGoal': 0,
-    'BadGoal': 1,
-    'GoodGoalMulti': 2,
-    'Wall': 3,
-    'Ramp': 4,
-    'CylinderTunnel': 5,
-    'WallTransparent': 6,
-    'CylinderTunnelTransparent': 7,
-    'Cardbox1': 8,
-    'Cardbox2': 9,
-    'UObject': 10,
-    'LObject': 11,
-    'LObject2': 12,
-    'DeathZone': 13,
-    'HotZone': 14,
-}
-
-num_classes = len(labels)
 
 
 def create_object_arena(target_path, arena_name, num_objects=7, time=250):
