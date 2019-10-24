aee649421506ca3ec6c8d54fc7cea1c075490bef
albertbou92 <albertbo@kth.se>
2019-10-24 14:47:10 +0200

    fix bug

--- animal/object_detection_module/object_model.py
+++ animal/object_detection_module/object_model.py
@@ -84,7 +84,7 @@ class ImpalaCNNObject(NNBase):
         if self.is_recurrent:
             x, rnn_hxs = self.gru(x, rnn_hxs)
 
-        return self.linear(x), F.softmax(self.linear(x), dim=1), rnn_hxs, x
+        return self.linear(x), F.sigmoid(self.linear(x)), rnn_hxs, x
 
 
 class ImpalaCNN(nn.Module):
